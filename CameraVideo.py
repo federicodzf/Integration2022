b105263860648af06db5d0275f345d167912db42
federicodzf <102984566+federicodzf@users.noreply.github.com>
2022-04-26 16:58:17 -0600

    PiCamera code

---
+++ CameraVideo.py
@@ -0,0 +1,28 @@
+# Note, I did not write this; I took bits and pieces from different sources - did add some of the comments, though
+
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32          # Establishes frame rate
+camera.rotation = 180          # Camera is positioned upside down initially, so the prior command rotates the image 180 degrees
+rawCapture = PiRGBArray(camera, size=(640, 480)) # Produced 3D red-green-blue array - pixels that create image
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
