d4ce48db6b62068f64a79b009024291483460ce5
cjsoren <104533629+cjsoren@users.noreply.github.com>
2022-04-27 15:52:15 -0600

    Update CameraVideo.py

--- CameraVideo.py
+++ CameraVideo.py
@@ -1,28 +1,31 @@
-# Note, I did not write this; I took bits and pieces from different sources - did add some of the comments, though
-
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32          # Establishes frame rate
-camera.rotation = 180          # Camera is positioned upside down initially, so the prior command rotates the image 180 degrees
-rawCapture = PiRGBArray(camera, size=(640, 480)) # Produced 3D red-green-blue array - pixels that create image
-# allow the camera to warmup
+camera.rotation = 180          # Camera is positioned upside down initially; prior command rotates the image 180 degrees
+rawCapture = PiRGBArray(camera, size=(640, 480)) # Produced 3D red-green-blue array - pixels necessary for image creation
+
+# Allow the camera to power-on and focus
 time.sleep(0.1)
-# capture frames from the camera
+
+# Capture the frames (images) from the camera and continuously update the image
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
+    # Grab the raw NumPy array representing the image, then initialize the timestamp and occupied/unoccupied text
     image = frame.array
-    # show the frame
+    
+    # Show the video frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
+    
+    # Clear the stream in preparation for the next frame
     rawCapture.truncate(0)
-    # if the `q` key was pressed, break from the loop
+    
+    # If the `q` key was pressed, break from the loop and end script
     if key == ord("q"):
         break
